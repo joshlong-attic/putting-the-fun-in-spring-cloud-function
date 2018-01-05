020b20488981d8047d5f729df8c0268dcf102091
Josh Long <starbuxman@gmail.com>
2018-01-05 00:40:03 -0800

    simple example

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -23,19 +23,19 @@ API_ID=$( echo $API_ID | jq -r '.id' )
 
 # 3.1 Now we're defining the surface of the API gateway. Create the root resource.
 
-RESOURCE_ROOT_ID=$( aws apigateway get-resources --rest-api-id $API_ID  )
+RESOURCE_ROOT_ID=$( aws apigateway get-resources --rest-api-id $API_ID --region ${REGION} )
 RESOURCE_ROOT_ID=$( echo ${RESOURCE_ROOT_ID} | jq -r '.items[].id'   )
 
 
 # 3.2 Create the path for the resource.
 
-RESOURCE_ID=$( aws apigateway create-resource  --rest-api-id ${API_ID} --parent-id ${RESOURCE_ROOT_ID}  --path-part ${ENDPOINT_PATH_PART} )
+RESOURCE_ID=$( aws apigateway create-resource  --rest-api-id ${API_ID} --parent-id ${RESOURCE_ROOT_ID}  --path-part ${ENDPOINT_PATH_PART} --region ${REGION} )
 RESOURCE_ID=$( echo ${RESOURCE_ID} |  jq -r '.id' )
 
 
 # 3.3 Add the method GET to the resource.
 
-aws apigateway put-method --rest-api-id ${API_ID} --resource-id ${RESOURCE_ID} --http-method GET --authorization-type NONE
+aws apigateway put-method --rest-api-id ${API_ID} --resource-id ${RESOURCE_ID} --http-method GET --authorization-type NONE --region ${REGION}
 
 
 # 3.4 set the lambda function as the destination for the POST method
@@ -43,10 +43,10 @@ aws apigateway put-method --rest-api-id ${API_ID} --resource-id ${RESOURCE_ID} -
 INTEGRATION_METHOD=GET
 INTEGRATION_URI=arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/arn:aws:lambda:${REGION}:${AWS_ACCOUNT_ID}:function:${FUNCTION_NAME}/invocations
 
-echo $INTEGRATION_URI
 
 aws apigateway put-integration \
     --rest-api-id ${API_ID} \
+    --region ${REGION} \
     --resource-id ${RESOURCE_ID} \
     --http-method ${INTEGRATION_METHOD} \
     --type AWS \
