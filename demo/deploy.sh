88e257324e82b6c2766bf5b82cf3b44d7cfc846f
Josh Long <starbuxman@gmail.com>
2018-01-05 01:57:16 -0800

    its alliiiiiive

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -24,37 +24,80 @@ aws lambda create-function \
 
 # 3.0 create the API gateway itself.
 
-API_ID=$( aws apigateway create-rest-api --name ${REST_API_NAME} --region ${REGION} )
-API_ID=$( echo $API_ID | jq -r '.id' )
+REST_API_ID=$( aws apigateway create-rest-api --name ${REST_API_NAME} --region ${REGION} )
+REST_API_ID=$( echo $REST_API_ID | jq -r '.id' )
 
 
 # 3.1 Now we're defining the surface of the API gateway. Create the root resource.
 
-RESOURCE_ROOT_ID=$( aws apigateway get-resources --rest-api-id $API_ID --region ${REGION} )
+RESOURCE_ROOT_ID=$( aws apigateway get-resources --rest-api-id $REST_API_ID --region ${REGION} )
 RESOURCE_ROOT_ID=$( echo ${RESOURCE_ROOT_ID} | jq -r '.items[].id'   )
 
 
 # 3.2 Create the path for the resource.
 
-RESOURCE_ID=$( aws apigateway create-resource  --rest-api-id ${API_ID} --parent-id ${RESOURCE_ROOT_ID}  --path-part ${ENDPOINT_PATH_PART} --region ${REGION} )
+RESOURCE_ID=$( aws apigateway create-resource  --rest-api-id ${REST_API_ID} --parent-id ${RESOURCE_ROOT_ID}  --path-part ${ENDPOINT_PATH_PART} --region ${REGION} )
 RESOURCE_ID=$( echo ${RESOURCE_ID} |  jq -r '.id' )
 
 
 # 3.3 Add the method GET to the resource.
 
-aws apigateway put-method --rest-api-id ${API_ID} --resource-id ${RESOURCE_ID} --http-method GET --authorization-type NONE --region ${REGION}
+aws apigateway put-method --rest-api-id ${REST_API_ID} --resource-id ${RESOURCE_ID} --http-method GET --authorization-type NONE --region ${REGION}
 
 
 # 3.4 set the lambda function as the destination for the POST method
 
-INTEGRATION_METHOD=GET
+METHOD=GET
 INTEGRATION_URI=arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/arn:aws:lambda:${REGION}:${AWS_ACCOUNT_ID}:function:${FUNCTION_NAME}/invocations
 
 aws apigateway put-integration \
-    --rest-api-id ${API_ID} \
+    --rest-api-id ${REST_API_ID} \
     --region ${REGION} \
     --resource-id ${RESOURCE_ID} \
-    --http-method ${INTEGRATION_METHOD} \
+    --http-method ${METHOD} \
     --type AWS \
-    --integration-http-method ${INTEGRATION_METHOD}  \
+    --integration-http-method ${METHOD}  \
     --uri ${INTEGRATION_URI}
+
+
+aws apigateway put-method-response \
+    --rest-api-id ${REST_API_ID} \
+    --resource-id ${RESOURCE_ID} \
+    --http-method $METHOD \
+    --region $REGION \
+    --status-code 200 \
+    --response-models "{\"application/json\": \"Empty\"}"
+
+aws apigateway put-integration-response \
+    --rest-api-id ${REST_API_ID} \
+    --resource-id ${RESOURCE_ID} \
+    --http-method $METHOD \
+    --region $REGION \
+    --status-code 200 \
+    --response-templates "{\"application/json\": \"\"}"
+
+
+# 3.5 deploy the API
+aws apigateway create-deployment --rest-api-id ${REST_API_ID} --stage-name prod --region $REGION
+
+# 3.6 grant permissions
+
+aws lambda add-permission \
+    --function-name ${FUNCTION_NAME} \
+    --statement-id ${REST_API_NAME}-dev  \
+    --action lambda:InvokeFunction \
+    --principal apigateway.amazonaws.com \
+    --region $REGION \
+    --source-arn "arn:aws:execute-api:${REGION}:${AWS_ACCOUNT_ID}:${REST_API_ID}/*/${METHOD}/${ENDPOINT_PATH_PART}"
+
+aws lambda add-permission \
+    --function-name ${FUNCTION_NAME} \
+    --statement-id ${REST_API_NAME}-prod  \
+    --action lambda:InvokeFunction \
+    --principal apigateway.amazonaws.com \
+    --region $REGION \
+    --source-arn "arn:aws:execute-api:${REGION}:${AWS_ACCOUNT_ID}:${REST_API_ID}/*/${METHOD}/${ENDPOINT_PATH_PART}"
+
+
+# 3.7
+#done!
