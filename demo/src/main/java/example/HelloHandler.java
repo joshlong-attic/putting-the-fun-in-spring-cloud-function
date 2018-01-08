e5f21830f5af9854c449f7287ebfa546eb9aae45
Josh Long <starbuxman@gmail.com>
2018-01-08 03:33:00 -0800

    mapped input parameters to JSON

--- demo/src/main/java/example/HelloHandler.java
+++ demo/src/main/java/example/HelloHandler.java
@@ -2,10 +2,8 @@ package example;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-import java.util.Map;
-
 /**
  * Look ma! No AWS! (or much anything else, really.)
  */
-public class HelloHandler extends SpringBootRequestHandler<In,  Out > {
+public class HelloHandler extends SpringBootRequestHandler<In, Out> {
 }
